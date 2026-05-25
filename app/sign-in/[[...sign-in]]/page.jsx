19fec0b05fcd88e394574d1fd3a92addcaffc842
yash-gautam9953 <yashgautam9953@gmail.com>
2026-05-25 11:00:18 +0530

    remove clerk

--- app/sign-in/[[...sign-in]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div style={{ display: "flex", minHeight: "100vh", alignItems: "center", justifyContent: "center" }}>
-      <SignIn />
-    </div>
-  );
-}
--- app/sign-up/[[...sign-up]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div style={{ display: "flex", minHeight: "100vh", alignItems: "center", justifyContent: "center" }}>
-      <SignUp />
-    </div>
-  );
-}
